161b50c6e9b7cbc19d8b996e68a535c68980b072
angari <uky00832@gmail.com>
2022-05-02 05:39:59 +0900

    파이썬 서버 구현 / 코멘트 DB 연결

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.campProject
+
+## HTML을 주는 부분
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/comment', methods=['GET'])
+def listing():
+    comments = list(db.instargramUsers.find({}, {'_id': False}))
+    return jsonify({'comments':comments})
+
+## API 역할을 하는 부분
+@app.route('/comment', methods=['POST'])
+def saving():
+    userReceive = request.form['userGive']
+    commentReceive = request.form['commentGive']
+
+    doc = {
+        'user': userReceive,
+        'comment': commentReceive
+    }
+    db.instargramUsers.insert_one(doc)
+
+    return jsonify()
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)
